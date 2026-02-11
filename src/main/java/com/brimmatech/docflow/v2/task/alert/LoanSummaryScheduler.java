e56c8166f9e91158cc7c0a230e916605792a4253
sathya2209 <sathya.selvi@brimmatech.com>
2026-02-11 14:50:07 +0530

    dottie

--- src/main/java/com/brimmatech/docflow/v2/task/alert/LoanSummaryScheduler.java
+++ src/main/java/com/brimmatech/docflow/v2/task/alert/LoanSummaryScheduler.java
@@ -25,28 +25,32 @@ public class LoanSummaryScheduler {
     private final TenantEntityRepository tenantEntityRepository;
     private final AdminService adminService;
 
+    @Value("${enable-loan-summary}")
+    private boolean sendEmail;
+
     @Scheduled(cron = "${loan-summary-cron}")
     public void runDailyLoanSummary() {
-        log.info("==== Loan Summary Scheduler STARTED at UTC:{} ====", ZonedDateTime.now(ZoneOffset.UTC));
-        try {
-            List<TenantEntity> eligibleTenantsForClassification =
-                    tenantEntityRepository.findAllBySendLoanSummaryTrue();
+        if (sendEmail) {
+            log.info("==== Loan Summary Scheduler STARTED at UTC:{} ====", ZonedDateTime.now(ZoneOffset.UTC));
+            try {
+                List<TenantEntity> eligibleTenantsForClassification =
+                        tenantEntityRepository.findAllBySendLoanSummaryTrue();
 
-            log.info("Eligible tenants count for classification : {} ", eligibleTenantsForClassification.size());
+                log.info("Eligible tenants count for classification : {} ", eligibleTenantsForClassification.size());
 
-            eligibleTenantsForClassification.forEach(tenant -> {
-                try {
-                    adminService.loanSummaryAttachmentEmail(tenant.getId());
-                } catch (Exception e) {
-                    log.error("Failed to send loan summary for tenant id {}: {}", tenant.getId(), e.getMessage(), e);
-                }
-            });
-        } catch (Exception ex) {
-            log.error("Unhandled exception in LoanSummaryScheduler: {}", ex.getMessage(), ex);
+                eligibleTenantsForClassification.forEach(tenant -> {
+                    try {
+                        adminService.loanSummaryAttachmentEmail(tenant.getId());
+                    } catch (Exception e) {
+                        log.error("Failed to send loan summary for tenant id {}: {}", tenant.getId(), e.getMessage(), e);
+                    }
+                });
+            } catch (Exception ex) {
+                log.error("Unhandled exception in LoanSummaryScheduler: {}", ex.getMessage(), ex);
+            }
+            log.info("==== Loan Summary Scheduler COMPLETED at UTC:{} ====", ZonedDateTime.now(ZoneOffset.UTC));
         }
-        log.info("==== Loan Summary Scheduler COMPLETED at UTC:{} ====", ZonedDateTime.now(ZoneOffset.UTC));
     }
-
 
 }
 
